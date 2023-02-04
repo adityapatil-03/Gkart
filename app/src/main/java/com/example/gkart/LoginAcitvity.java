0fd93873bbb45c33647fd0bac58e69cad7663c71
suraj <surajmalharimali@gmail.com>
2023-02-04 22:05:19 +0530

    first login page design

---
+++ app/src/main/java/com/example/gkart/LoginAcitvity.java
@@ -0,0 +1,14 @@
+package com.example.gkart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginAcitvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_acitvity);
+    }
+}
