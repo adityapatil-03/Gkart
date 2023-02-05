6022eb2d6b16293105e7137be3983f8177db1a24
pranav <rukadepranav@gmail.com>
2023-02-05 19:39:30 +0530

    working login page

--- app/src/main/java/com/example/gkart/LoginActivity.java
+++ app/src/main/java/com/example/gkart/LoginActivity.java
@@ -1,14 +1,64 @@
 package com.example.gkart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText username;
+    private EditText password;
+    private Button loginbutton;
+    private FirebaseAuth authenticator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
         setContentView(R.layout.activity_login_acitvity);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginbutton = findViewById(R.id.loginbutton);
+        authenticator = FirebaseAuth.getInstance();
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = username.getText().toString();
+                String pass = password.getText().toString();
+                if (email.length()>0&&pass.length()>0){
+                    loginUser(email,pass);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"OOOps!! Empty credentials..",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void loginUser(String name,String pass){
+        authenticator.signInWithEmailAndPassword(name,pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LoginActivity.this,"Hurrrayy! Login was successful",Toast.LENGTH_SHORT).show();
+//                start new intent here
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this,"OOOps! Login failed",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
